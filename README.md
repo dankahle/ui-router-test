1f111e2eed5af090288356590eaad960729ff45f
Dan Kahle <daniel.kahle@acxiom.com>
2016-10-04 13:46:42 -0400

    latest

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 #### notable
 * when you throw in a resolve, doesn't go to otherwise, just fails to get to that state
 * when you event.preventDefault() in a $stateChangeStart handler, it doesn't go to otherwise either, just fails to get to that state
+* test/:id will have no empty string id for /test/ url but still run the controller
--- app/app.js
+++ app/app.js
@@ -7,7 +7,10 @@ angular.module('app', ['ui.router'])
         .state({
             name: 'home',
             url: '/home',
-            template: 'home state'
+            template: 'home state',
+            controller: function() {
+                console.log('home ctrl')
+            }
         })
         .state({
             name: 'about',
@@ -21,6 +24,39 @@ angular.module('app', ['ui.router'])
             // }
         })
         .state({
+            name: 'test',
+            url: '/test/:id',
+            template: 'test state',
+            controller: function() {
+                console.log('test ctrl')
+            }
+        })
+        .state({
+            name: 'parent',
+            url: '/parent',
+            template: '<div>parent state</div><ui-view></ui-view>',
+            controller: function($state) {
+                console.log('parent ctrl');
+                $state.go('parent.child1');
+            }
+        })
+        .state({
+            name: 'parent.child1',
+            url: '/parent/child1',
+            template: '<div>child1 state</div>',
+            controller: function() {
+                console.log('child1 ctrl')
+            }
+        })
+        .state({
+            name: 'parent.child2',
+            url: '/parent/child2',
+            template: '<div>child2 state</div>',
+            controller: function() {
+                console.log('child2 ctrl')
+            }
+        })
+        .state({
             name: 'other',
             url: '/other',
             template: 'other state'
@@ -31,19 +67,20 @@ angular.module('app', ['ui.router'])
 
 
 })
-.run(function($rootScope) {
+.run(function($rootScope, $stateParams) {
 
     $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
         console.error('Failed to change to state: %s\nError: %s', toState.name, error);
     });
 
     $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams) {
-        console.log('$stateChangeSuccess', toState.name);
+        console.log('$stateChangeSuccess', toState.name, $stateParams);
     });
 
     $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
         console.log('$stateChangeStart', toState.name, toParams);
         if (toState.name === 'about') {
+            console.log('preventDefault()');
             event.preventDefault();
         }
     });
