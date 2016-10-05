b5475da6d47e11af466322fefd6fb959276628ca
Dan Kahle <daniel.kahle@acxiom.com>
2016-10-05 11:46:24 -0400

    latest

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 * when you throw in a resolve, doesn't go to otherwise, just fails to get to that state
 * when you event.preventDefault() in a $stateChangeStart handler, it doesn't go to otherwise either, just fails to get to that state
 * test/:id will have no empty string id for /test/ url but still run the controller
+* going from a child to a parent will not rerun parent's controller
+* ****** the issue that killed you: when you event.preventDefault() on otherwise state (state not found), ui-router just starts another otherwise state (what?). The only way around it is to state.go inside that preventDefault handling code, then it shuts up. Hence the init state added, a state to sit it while the initialization promises get resolved.
--- app/app.js
+++ app/app.js
@@ -1,93 +1,130 @@
-
-
 angular.module('app', ['ui.router'])
-.config(function($stateProvider, $urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-    $stateProvider
-        .state({
-            name: 'home',
-            url: '/home',
-            template: 'home state',
-            controller: function() {
-                console.log('home ctrl')
-            }
-        })
-        .state({
-            name: 'about',
-            url: '/about',
-            template: 'about state'
-            // this resolve fail will log via stateChangeError message, but will NOT forward to otherwise state
-            // resolve: {
-            //     fail: function() {
-            //         throw 'lala';
-            //     }
-            // }
-        })
-        .state({
-            name: 'test',
-            url: '/test/:id',
-            template: 'test state',
-            controller: function() {
-                console.log('test ctrl')
-            }
-        })
-        .state({
-            name: 'parent',
-            url: '/parent',
-            template: '<div>parent state</div><ui-view></ui-view>',
-            controller: function($state) {
-                console.log('parent ctrl');
-                $state.go('parent.child1');
-            }
-        })
-        .state({
-            name: 'parent.child1',
-            url: '/parent/child1',
-            template: '<div>child1 state</div>',
-            controller: function() {
-                console.log('child1 ctrl')
-            }
-        })
-        .state({
-            name: 'parent.child2',
-            url: '/parent/child2',
-            template: '<div>child2 state</div>',
-            controller: function() {
-                console.log('child2 ctrl')
-            }
-        })
-        .state({
-            name: 'other',
-            url: '/other',
-            template: 'other state'
+        $stateProvider
+            .state({
+                name: 'init'
+            })
+            .state({
+                name: 'home',
+                url: '/home',
+                template: 'home state',
+                controller: function () {
+                    console.log('home ctrl')
+                }
+            })
+            .state({
+                name: 'fail',
+                url: '/fail',
+                template: 'fail state'
+                // this resolve fail will log via stateChangeError message, but will NOT forward to otherwise state
+                // resolve: {
+                //     fail: function() {
+                //         throw 'lala';
+                //     }
+                // }
+            })
+            .state({
+                name: 'test',
+                url: '/test/:id',
+                template: 'test state',
+                controller: function () {
+                    console.log('test ctrl')
+                }
+            })
+            .state({
+                name: 'parent',
+                url: '/parent',
+                template: '<div>parent state</div><ui-view></ui-view>',
+                controller: function ($rootScope, $state, Global) {
+                    console.log('parent ctrl');
+                }
+            })
+            .state({
+                name: 'parent.child1',
+                url: '/parent/child1',
+                template: '<div>child1 state</div>',
+                controller: function () {
+                    console.log('child1 ctrl')
+                }
+            })
+            .state({
+                name: 'parent.child2',
+                url: '/parent/child2',
+                template: '<div>child2 state</div>',
+                controller: function () {
+                    console.log('child2 ctrl')
+                }
+            })
+            .state({
+                name: 'other',
+                url: '/other',
+                template: 'other state'
+            });
+
+        $urlRouterProvider
+            .otherwise('/parent');
+
+
+    })
+    .value('Global', {val: undefined})
+
+    .run(function ($rootScope, $stateParams, Global, $state, $timeout) {
+
+        $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+            console.error('Failed to change to state: %s\nError: %s', toState.name, error);
         });
 
-    $urlRouterProvider
-        .otherwise('/other');
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams) {
+            console.log('$stateChangeSuccess', toState.name, $stateParams);
+        });
 
-
-})
-.run(function($rootScope, $stateParams) {
-
-    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-        console.error('Failed to change to state: %s\nError: %s', toState.name, error);
-    });
-
-    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams) {
-        console.log('$stateChangeSuccess', toState.name, $stateParams);
-    });
-
-    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
-        console.log('$stateChangeStart', toState.name, toParams);
-        if (toState.name === 'about') {
-            console.log('preventDefault()');
-            event.preventDefault();
-        }
-    });
-
-})
-.controller('mainCtrl', function() {
+        $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+            console.log('$stateChangeStart', toState.name, toParams);
 
 
 
-})
+            // if (toState.name === 'fail') {
+            //     console.log('preventDefault()');
+            //     event.preventDefault();
+            // }
+
+            if (toState.name === 'init') {
+                return;
+            }
+
+            if (!Global.init) {
+                console.log('init preventDefault()');
+                event.preventDefault();
+                $state.go('init');
+
+                $timeout(function() {
+                    Global.init = true;
+                    $state.go(toState.name, toParams);
+                },1000);
+                return;
+            }
+
+
+            if (toState.name == 'parent') {
+                console.log('parent preventDefault()');
+                event.preventDefault();
+
+                $timeout(function() {
+                    if (Global.val == '1')
+                        $state.go('parent.child1');
+                    else
+                        $state.go('parent.child2');
+                }, 1000);
+            }
+
+        });
+
+
+    })
+
+    .controller('mainCtrl', function ($scope, Global) {
+
+        $scope.Global = Global;
+
+    })
